b74117f0b9390e9450e1a3e8515d144c28d45088
Samanvita S <samanvitas.ad24@bmsce.ac.in>
2025-11-11 14:35:42 +0530

    Add files via upload

---
+++ circular queue.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+#define SIZE 6
+
+int items[SIZE];
+int front = -1, rear = -1;
+
+int isFull() {
+    if ((front == rear + 1) || (front == 0 && rear == SIZE - 1))
+        return 1;
+    return 0;
+}
+
+int isEmpty() {
+    if (front == -1)
+        return 1;
+    return 0;
+}
+
+void enQueue(int element) {
+    if (isFull()) {
+        printf("\nQueue is full !!\n");
+    } else {
+        if (front == -1)
+            front = 0;
+        rear = (rear + 1) % SIZE;
+        items[rear] = element;
+        printf("\nInserted -> %d\n", element);
+    }
+}
+
+int deQueue() {
+    int element;
+    if (isEmpty()) {
+        printf("\nQueue is empty !!\n");
+        return -1;
+    } else {
+        element = items[front];
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        } else {
+            front = (front + 1) % SIZE;
+        }
+        printf("\nDeleted element -> %d\n", element);
+        return element;
+    }
+}
+
+void display() {
+    int i;
+    if (isEmpty()) {
+        printf("\nEmpty Queue\n");
+    } else {
+        printf("\nFront -> %d", front);
+        printf("\nItems -> ");
+        for (i = front; i != rear; i = (i + 1) % SIZE) {
+            printf("%d ", items[i]);
+        }
+        printf("%d ", items[i]);
+        printf("\nRear -> %d\n", rear);
+    }
+}
+
+int main() {
+    int choice, value;
+
+    while (1) {
+        printf("\n\n--- Circular Queue Menu ---\n");
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to enqueue: ");
+                scanf("%d", &value);
+                enQueue(value);
+                break;
+
+            case 2:
+                deQueue();
+                break;
+
+            case 3:
+                display();
+                break;
+
+            case 4:
+                printf("\nExiting...\n");
+                return 0;
+
+            default:
+                printf("\nInvalid choice! Please try again.\n");
+        }
+    }
+}
